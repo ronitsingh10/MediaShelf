b8d5ae30b874d737e3fc0f1d64ed20c40d17ab47
Ronit Singh <ronit@Ronits-MacBook-Air.local>
2025-04-30 02:23:52 -0400

    Somemore fixes for Vercel

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import SignInForm from "@/components/auth/sign-in-form";
 
@@ -5,21 +7,6 @@ const SignIn = () => {
   return (
     <div className="space-y-8">
       <div className="space-y-2">
-        {/* <div className="h-12 w-12 bg-emerald-800 rounded-lg flex items-center justify-center"> */}
-        {/* <svg
-            className="w-6 h-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg> */}
-        {/* </div> */}
         <h1 className="text-2xl font-semibold">Welcome back</h1>
         <p className="text-sm text-muted-foreground">
           Sign in to your MediaShelf account
